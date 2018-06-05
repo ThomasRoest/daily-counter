9bda6fb2c09e6bfc7297fbb5d7723d6979566752
thomasroest <thomasroest@outlook.com>
2018-06-05 09:15:43 +0200

    add localstorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
 
-const Counter = ({id, day, count, handleClick}) =>
-    <div className="counter">
-      <span className="counter-item">{day}</span>
-      <span className="counter-item counter-count">{count}</span>
-      <button className="counter-item btn btn-primary"onClick={(e) => handleClick(e, id)}>Click</button>
-    </div>
+
+class ListItem extends Component {
+  render() {
+    if(!this.props) return null;
+    const { id, day, count, handleClick } = this.props;
+    return (
+      <div className="counter">
+        <span className="counter-item">{day}</span>
+        <span className="counter-item counter-count">{count}</span>
+        <button
+          className="counter-item btn btn-primary"
+          onClick={e => handleClick(e, id)}
+        >
+          Click
+        </button>
+      </div>
+    )
+  }
+}
+
 
 class App extends Component {
   constructor(props) {
@@ -14,39 +28,55 @@ class App extends Component {
     this.handleClick = this.handleClick.bind(this);
     this.addNewDay = this.addNewDay.bind(this);
     this.state = {
-      days: [{ id: 1, date: new Date(), count: 0 }, {id: 2, date: new Date(), count: 50 }]
+      days: []
     };
   }
 
+  componentDidMount() {
+    const localStorageRef = localStorage.getItem('daily-counter-app')
+    if(localStorageRef) {
+      this.setState({ days: JSON.parse(localStorageRef) })
+    }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('daily-counter-app', JSON.stringify(this.state.days))
+  }
+
   handleClick(e, id) {
-    const days = [...this.state.days]
-    const updatedItem = days.find(item => item.id === id)
-    updatedItem.count++
-    this.setState({ days: days})
+    const days = [...this.state.days];
+    const updatedItem = days.find(item => item.id === id);
+    updatedItem.count++;
+    this.setState({ days: days });
   }
 
   addNewDay() {
-    const newDay = { id: Date.now(), date: new Date(), count: 0 };
-    const updateddays = [...this.state.days, newDay]
-    this.setState({days: updateddays})
+    const newDay = { id: Date.now(), date: 'date must be string', count: 0 };
+    const updateddays = [...this.state.days, newDay];
+    this.setState({ days: updateddays });
   }
- 
+
   render() {
     return (
       <div className="App">
         <header className="App-header">counter</header>
         <div>
           {this.state.days.map(day => (
-            <Counter
+            <ListItem
               id={day.id}
               key={day.id}
-              day={day.date.toDateString()}
+              day={day.date}
               count={day.count}
               handleClick={this.handleClick}
             />
           ))}
         </div>
-        <button className="btn btn-success btn-lg btn-addnew" onClick={this.addNewDay}>add new day</button>
+        <button
+          className="btn btn-success btn-lg btn-addnew"
+          onClick={this.addNewDay}
+        >
+          add new day
+        </button>
       </div>
     );
   }
